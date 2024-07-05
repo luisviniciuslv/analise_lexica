1d62357ecd33abe63d274799b9dfbe9cd21bf8fc
Vini <luisvpersy@gmail.com>
2024-07-05 16:47:22 -0300

    nice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+#[derive(Debug, Clone, Copy, PartialEq)] // Eu não entendi o que é isso
+pub enum Token {
+    Number(u32),
+    Plus,
+    Minus,
+    Star,
+    Slash
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = "1 + 2 * 3";
+    let tokens = token(s);
+    println!("{:?}", tokens);
+}
+
+fn token(s: &str) -> Vec<Token> {
+    let mut vec_token = Vec::new();
+    let mut s = s.chars();
+
+    while let Some(c) = s.next() {
+        println!("{}", c);
+        match c {
+            ' ' => {
+                // Skip
+            }
+            '0'..='9' => {
+                vec_token.push(Token::Number(c.to_digit(10).unwrap()));
+            }
+            '+' => {
+                vec_token.push(Token::Plus );
+            }
+            '-' => {
+                vec_token.push(Token::Minus );
+            }
+            '*' => {
+                vec_token.push(Token::Star );
+            }
+            '/' => {
+                vec_token.push(Token::Slash );
+            }
+            _ => {
+                panic!("Invalid character found: {}", c);
+            }
+        }
+    }
+    return vec_token
 }
