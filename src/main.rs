ec9a831033b352b4215da3aa535d09890820513f
Vini <luisvpersy@gmail.com>
2024-07-05 18:01:52 -0300

    lexer da  @imaqtkatt

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,71 @@
-#[derive(Debug, Clone, Copy, PartialEq)] // Eu não entendi o que é isso
+#[derive(Debug)]
 pub enum Token {
-    Number(u32),
-    Plus,
-    Minus,
-    Star,
-    Slash
+  Number(u32),
+  Plus,
+  Minus,
+  Star,
+  Slash,
+}
+
+#[derive(Debug)]
+pub enum LexState {
+  Default,
+  Number,
+  Error(char),
+  Stop,
+}
+
+fn tokens(s: &str) -> Vec<Token> {
+  let mut lex_state = LexState::Default;
+  let mut number = String::new();
+  let mut tokens = Vec::new();
+  let mut cursor = 0;
+
+  'lex: loop {
+    match &lex_state {
+      LexState::Default => {
+        match s.chars().nth(cursor) {
+          Some(' ') => {}
+          Some('+') => tokens.push(Token::Plus),
+          Some('-') => tokens.push(Token::Minus),
+          Some('*') => tokens.push(Token::Star),
+          Some('/') => tokens.push(Token::Slash),
+          Some(c) if c.is_ascii_digit() => {
+            number.push(c);
+            lex_state = LexState::Number;
+          }
+          Some(c) => lex_state = LexState::Error(c),
+          None => lex_state = LexState::Stop,
+        }
+        cursor += 1;
+      }
+      LexState::Number => match s.chars().nth(cursor) {
+        Some(c) if c.is_ascii_digit() => {
+          number.push(c);
+          cursor += 1;
+          continue 'lex;
+        }
+        Some(_other) => {
+          tokens.push(Token::Number(number.parse().unwrap()));
+          number = String::new();
+          lex_state = LexState::Default;
+          continue 'lex;
+        }
+        None => {
+          tokens.push(Token::Number(number.parse().unwrap()));
+          number = String::new();
+          lex_state = LexState::Stop;
+          continue 'lex;
+        }
+      },
+      LexState::Error(c) => panic!("Unexpected character '{c}'"),
+      LexState::Stop => break,
+    }
+  }
+
+  tokens
 }
 
 fn main() {
-    let s = "11+ 2 * 33";
-    let tokens = token(s);
-    println!("{:?}", tokens);
-}
-
-fn token(s: &str) -> Vec<Token> {
-    let mut vec_token = Vec::new();
-    let mut s = s.chars();
-    let mut current_number = String::new();
-    let mut is_number = false;
-    
-    fn push (token: Token, current_number: &mut String, vec_token: &mut Vec<Token>) {
-        if !current_number.is_empty() {
-            vec_token.push(Token::Number(current_number.parse().unwrap()));
-            current_number.clear();
-        } 
-        vec_token.push(token);
-    }
-
-    while let Some(c) = s.next() {
-        is_number = false;
-        match c {
-            ' ' => { 
-                if !current_number.is_empty() {
-                    vec_token.push(Token::Number(current_number.parse().unwrap()));
-                    current_number.clear();
-                }
-             }
-            '0'..='9' => {
-                is_number = true;
-                current_number.push(c);
-            }
-            '+' => {
-                push(Token::Plus, &mut current_number, &mut vec_token)
-            }
-            '-' => {
-                push(Token::Minus, &mut current_number, &mut vec_token)
-            }
-            '*' => {
-                push(Token::Star, &mut current_number, &mut vec_token)
-            }
-            '/' => {
-                push(Token::Slash, &mut current_number, &mut vec_token)
-            }
-            _ => {
-                panic!("Invalid character found: {}", c);
-            }
-        }
-
-
-        if !is_number && !current_number.is_empty() {
-            vec_token.push(Token::Number(current_number.parse().unwrap()));
-            current_number.clear();
-        }
-    }
-    
-    if !current_number.is_empty() { // Adiciona o último número se houver
-        vec_token.push(Token::Number(current_number.parse().unwrap()));
-    }
-
-    return vec_token;
+  println!("{:?}", tokens("42 3 + 5 - 2 * 4  /"));
 }
