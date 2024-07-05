6563bcb5bb337f01a2dd8204ecc77c088be4408b
Vini <luisvpersy@gmail.com>
2024-07-05 17:02:14 -0300

    números maiores que 9

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub enum Token {
 }
 
 fn main() {
-    let s = "1 + 2 * 3";
+    let s = "11 + 2 * 3";
     let tokens = token(s);
     println!("{:?}", tokens);
 }
@@ -16,15 +16,18 @@ fn main() {
 fn token(s: &str) -> Vec<Token> {
     let mut vec_token = Vec::new();
     let mut s = s.chars();
+    let mut current_number = String::new();
 
     while let Some(c) = s.next() {
-        println!("{}", c);
         match c {
             ' ' => {
-                // Skip
+                if !current_number.is_empty() {
+                    vec_token.push(Token::Number(current_number.parse().unwrap()));
+                    current_number.clear();
+                }
             }
             '0'..='9' => {
-                vec_token.push(Token::Number(c.to_digit(10).unwrap()));
+                current_number.push(c);
             }
             '+' => {
                 vec_token.push(Token::Plus );
@@ -43,5 +46,10 @@ fn token(s: &str) -> Vec<Token> {
             }
         }
     }
+
+    if !current_number.is_empty() { // Adiciona o último número se houver
+        vec_token.push(Token::Number(current_number.parse().unwrap()));
+    }
+    
     return vec_token
 }
